c5092469327713e40715643761355173b4d06125
Your Name <you@example.com>
2020-04-14 20:34:16 +0900

    first commit

---
+++ core.py
@@ -0,0 +1,42 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+
+OPENCV_OBJECT_TRACKERS = {
+    "csrt": cv2.TrackerCSRT_create,
+    "kcf": cv2.TrackerKCF_create,
+    "boosting": cv2.TrackerBoosting_create,
+    "mil": cv2.TrackerMIL_create,
+    "tld": cv2.TrackerTLD_create,
+    "medianflow": cv2.TrackerMedianFlow_create,
+    "mosse": cv2.TrackerMOSSE_create
+}
+
+def rsInit():
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    pipeline.start(config)
+    return pipeline
+
+def select_ROI(color_image):
+    cv2.namedWindow('select ROI', cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('select ROI', color_image)
+    cv2.putText(color_image ,"select ROI", (0, 10),cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+    rect = cv2.selectROI("select ROI",color_image)
+    cv2.destroyWindow("select ROI")
+    tracker = OPENCV_OBJECT_TRACKERS['mil']()
+    tracker.init(color_image,rect)
+    return tracker
+
+def tracking(color_image,depth_frame,tracker):
+    _, box = tracker.update(color_image)
+    left,top,w,h = [int(v) for v in box]
+    right = left + w
+    bottom = top + h
+    x_middle = (left + right) // 2
+    y_middle = (top + bottom) // 2
+    cv2.rectangle(color_image,(left,top),(right,bottom),(255,255,255),3)
+    depth = depth_frame.get_distance(x_middle,y_middle)
+    return color_image,depth
---
+++ main.py
@@ -0,0 +1,49 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+from core import rsInit,select_ROI,tracking
+
+pipeline = rsInit()
+isTracker = False
+
+try:
+    while True:
+        # 카메라 읽는 부분 START
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        if not depth_frame or not color_frame:
+            continue
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+        # 카메라 읽는 부분 END
+        ##### LOGIC START
+
+
+
+
+        if isTracker == False:
+            tracker = select_ROI(color_image)
+            isTracker = True
+        color_image,depth = tracking(color_image,depth_frame,tracker)
+        print(depth)
+        cv2.putText(color_image ,"{}cm".format(depth*100), (0, 10),cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+        
+
+
+
+
+        ##### LOGIC END
+        # 화면에 보여주는 부분 START
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', color_image)
+        # 화면에 보여주는 부분 END
+
+        # q 키 누르면 끄는 코드
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q') or key == 27:
+            cv2.destroyAllWindows()
+            break
+
+finally:
+    pass
